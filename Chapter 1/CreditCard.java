357ce50af69f9836c5c5ec46551c41e7a853379c
cdelfattore <cdelfattore@live.com>
2025-02-07 14:18:24 -0500

    Finished the Reinforcement section of chapter 1

--- Chapter 1/CreditCard.java
+++ Chapter 1/CreditCard.java
@@ -63,7 +63,10 @@ public class CreditCard
 
     public void makePayment(double amount)
     {
-        this.balance -= amount;
+        if (amount > 0) //added if statement for R_1_12
+        {
+            this.balance -= amount;
+        }
     }
 
     /**
@@ -82,6 +85,29 @@ public class CreditCard
         System.out.println("Account = " + card.account);
         System.out.println("Balance = " + card.balance);
         System.out.println("Limit = " + card.limit);
+    }
+
+    public static void main(String[ ] args) {
+        CreditCard[] wallet = new CreditCard[3];
+        wallet[0] = new CreditCard("John Bowman", "California Savings", "5391 0375 9387 5309", 5000);
+        wallet[1] = new CreditCard("John Bowman", "California Federal", "3485 0399 3395 1954", 3500);
+        wallet[2] = new CreditCard("John Bowman", "California Finance", "5391 0375 9387 5309", 2500, 300);
+    
+        for (int val = 1; val <= 16; val++) {
+            wallet[0].charge(100*val);
+            wallet[1].charge(2*val);
+            wallet[2].charge(val);
+        }
+
+        for (CreditCard card : wallet) 
+        {
+            CreditCard.printSummary(card); // calling static method
+            while (card.getBalance( ) > 200.0)
+            {
+                card.makePayment(200);
+                System.out.println("New balance = " + card.getBalance());
+            }
+        }
     }
 
 }
